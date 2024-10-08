a617594890975fe39138f71f914ab85cd0a4f990
meg1106 <theguanying@gmail.com>
2024-10-08 22:36:32 +0800

    feat: add nodemon

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World11111122222');
 });
 
 app.post('/regist', (req, res) => {
